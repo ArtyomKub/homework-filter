36e8ff3915bdeb8a38aaabb5dc7aed347b66ee61
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-22 18:38:10 +0200

    Copy function Filter from App.tsx in NewComponent.tsx (with many errors)

--- src/App.tsx
+++ src/App.tsx
@@ -33,24 +33,9 @@ function App() {
     }
 
     return (
-        <>
-            <ul>
-                {currentMoney.map((moneyArray, index) => {
-                    return (
-                        <li key={index}>
-                            <span>{moneyArray.banknots} -</span>
-                            <span>{moneyArray.value} -</span>
-                            <span>{moneyArray.number}</span>
-                        </li>
-                    )
-                })}
-            </ul>
-            <div className={'buttonClass'}>
-                <button onClick={() => onClickHandler('Dollars')}>Dollars</button>
-                <button onClick={() => onClickHandler('Rubles')}>Rubles</button>
-                <button onClick={() => onClickHandler('All')}>All</button>
-            </div>
-        </>
+       <div>
+
+       </div>
     );
 }
 
--- src/NewComponent.tsx
+++ src/NewComponent.tsx
@@ -2,8 +2,23 @@ import React from 'react';
 
 export const NewComponent = () => {
     return (
-        <div>
-
-        </div>
+        <>
+            <ul>
+                {currentMoney.map((moneyArray, index) => {
+                    return (
+                        <li key={index}>
+                            <span>{moneyArray.banknots} -</span>
+                            <span>{moneyArray.value} -</span>
+                            <span>{moneyArray.number}</span>
+                        </li>
+                    )
+                })}
+            </ul>
+            <div className={'buttonClass'}>
+                <button onClick={() => onClickHandler('Dollars')}>Dollars</button>
+                <button onClick={() => onClickHandler('Rubles')}>Rubles</button>
+                <button onClick={() => onClickHandler('All')}>All</button>
+            </div>
+        </>
     );
 };
