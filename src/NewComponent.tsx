0ec0ad7050aa9ed120d4643b629e799b2c670b17
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-22 19:16:52 +0200

    Complete typing in NewComponent.tsx

--- src/NewComponent.tsx
+++ src/NewComponent.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
 export type NewComponentType = {
-    onClickHandler: (nameButton: string) => void
-    currentMoney: Array<CurrentMoneyType>
+    currentMoney: Array<currentMoneyType>
+    onClickHandler: (nameButton:string)=>void
+
 }
 
-export type CurrentMoneyType = {
+
+
+type onClickHandlerType = {
+    setFilter: (nameButton:string) => void
+}
+
+type currentMoneyType = {
     banknots: string
     value: number
     number: string
@@ -15,7 +22,7 @@ export const NewComponent = (props: NewComponentType) => {
     return (
         <>
             <ul>
-                {currentMoney.map((moneyArray, index) => {
+                {props.currentMoney.map((moneyArray, index) => {
                     return (
                         <li key={index}>
                             <span>{moneyArray.banknots} -</span>
